5a47f51a66a5adba71aba70f49114227bb5a78dc
eschiavo <eric.schiavo@appian.com>
2016-08-29 15:59:26 -0400

    Version 1.0.0 of Help application

---
+++ Help/Help Release 1.0.0/SQL Scripts/mySQL/Help Release 1.0.0 Insert Reference Data.sql
@@ -0,0 +1,12 @@
+/* *** Help Application Version 1.0.0 - Insert Reference Data *** */
+
+INSERT INTO `HELP_R_RSC_TYPE` (`R_TYPE_VAL`, `R_TYPE_ORD`, `R_TYPE_ACTV_FLG`) VALUES ('Payment Excellence', 1, 1);
+INSERT INTO `HELP_R_RSC_TYPE` (`R_TYPE_VAL`, `R_TYPE_ORD`, `R_TYPE_ACTV_FLG`) VALUES ('Network Excellence', 2, 1);
+
+INSERT INTO `HELP_R_RSC_CAT` (`R_CAT_VAL`, `R_CAT_ORD`, `R_CAT_ACTV_FLG`) VALUES ('Training', 1, 1);
+
+INSERT INTO `HELP_R_RSC_STS` (`R_STS_CD`, `R_STS_VAL`, `R_STS_ORD`, `R_STS_ACTV_FLG`) VALUES ('P', 'Pending', 1, 0);
+INSERT INTO `HELP_R_RSC_STS` (`R_STS_CD`, `R_STS_VAL`, `R_STS_ORD`, `R_STS_ACTV_FLG`) VALUES ('S', 'Sent Back for Revision', 2, 0);
+INSERT INTO `HELP_R_RSC_STS` (`R_STS_CD`, `R_STS_VAL`, `R_STS_ORD`, `R_STS_ACTV_FLG`) VALUES ('A', 'Active', 3, 1);
+INSERT INTO `HELP_R_RSC_STS` (`R_STS_CD`, `R_STS_VAL`, `R_STS_ORD`, `R_STS_ACTV_FLG`) VALUES ('R', 'Rejected', 4, 0);
+INSERT INTO `HELP_R_RSC_STS` (`R_STS_CD`, `R_STS_VAL`, `R_STS_ORD`, `R_STS_ACTV_FLG`) VALUES ('I', 'Inactive', 5, 1);
