43a00a66c23a63c781045545532dff4b1b75a2c5
Eric Schiavo <eric.schiavo@appian.com>
2016-10-07 15:15:10 -0400

    Version 1.1.0 applications and database files

---
+++ Help/Help Release 1.1.0/Database/Help Release 1.1.0 Insert Reference Data.sql
@@ -0,0 +1,7 @@
+/* *** Help Application Version 1.1.0 - Insert Reference Data *** */
+
+INSERT INTO `HELP_R_RSC_STS` (`R_STS_CD`, `R_STS_VAL`, `R_STS_ORD`, `R_STS_ACTV_FLG`) VALUES ('P', 'Pending', 1, 0);
+INSERT INTO `HELP_R_RSC_STS` (`R_STS_CD`, `R_STS_VAL`, `R_STS_ORD`, `R_STS_ACTV_FLG`) VALUES ('S', 'Sent Back for Revision', 2, 0);
+INSERT INTO `HELP_R_RSC_STS` (`R_STS_CD`, `R_STS_VAL`, `R_STS_ORD`, `R_STS_ACTV_FLG`) VALUES ('A', 'Active', 3, 1);
+INSERT INTO `HELP_R_RSC_STS` (`R_STS_CD`, `R_STS_VAL`, `R_STS_ORD`, `R_STS_ACTV_FLG`) VALUES ('R', 'Rejected', 4, 0);
+INSERT INTO `HELP_R_RSC_STS` (`R_STS_CD`, `R_STS_VAL`, `R_STS_ORD`, `R_STS_ACTV_FLG`) VALUES ('I', 'Inactive', 5, 1);
